4709ee6e2452696fd2682ad5223a0dc5d2d1095c
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-04 00:08:50 +0300

    create model Product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from main.models import Company
 
-# Create your models here.
+
+class Product(models.Model):
+    """Модель продукта
+    - name: Наименование
+    - model: Модель продукта
+    - release_date: Дата выхода на рынок
+    - company: Компания в которой находится продукт, связаная модель (Company)
+    - quantity: Количество продукта
+    - price: Цена"""
+
+    name = models.CharField(max_length=255, verbose_name="product_name")
+    model = models.CharField(max_length=255, verbose_name="product_model")
+    release_date = models.DateField(verbose_name="product_release_date")
+    company = models.ForeignKey(Company,
+                                verbose_name="company_name",
+                                related_name="company_products",
+                                on_delete=models.CASCADE)
