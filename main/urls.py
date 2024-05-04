08ae3afe9e155d008c27449f00a6db3b4df1bdee
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-04 19:19:02 +0300

    add CompanyUpdateSerializer in urls

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from main.apps import MainConfig
 from main.views import (CompanyCreateAPIView, CompanyListAPIView, ContactCreateAPIView,
-                        CompanyDetailAPIView)
+                        CompanyDetailAPIView, CompanyUpdateAPIView)
 
 app_name = MainConfig.name
 
@@ -10,6 +10,7 @@ urlpatterns = [
     path('create/', CompanyCreateAPIView.as_view(), name='create-company'),
     path('list/', CompanyListAPIView.as_view(), name='list-company'),
     path('company/<int:pk>/', CompanyDetailAPIView.as_view(), name='profile-company'),
+    path('update/<int:pk>/', CompanyUpdateAPIView.as_view(), name='update-company'),
 
     #  Contacts urls
     path('contact/create/<int:pk>/', ContactCreateAPIView.as_view(), name='create-contacts'),
