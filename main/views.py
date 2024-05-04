3297157df75eefab7c471a8c5d5efa78a828125a
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-04 18:59:58 +0300

    create ContactCreateAPIView & CompanyListAPIView

--- main/views.py
+++ main/views.py
@@ -1,9 +1,37 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-from main.models import Company
-from main.serializers import CompanySerializer
+from main.models import Company, Contact
+from main.serializers import CompanySerializer, ContactSerializer
 
 
 class CompanyCreateAPIView(generics.CreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+class CompanyListAPIView(generics.ListAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+class ContactCreateAPIView(generics.CreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        company = Company.objects.filter(id=self.kwargs['pk']).first()
+        if company is not None:
+            contact = Contact.objects.create(
+                email=serializer.data['email'],
+                country=serializer.data['country'],
+                city=serializer.data['city'],
+                street=serializer.data['street'],
+                house_number=serializer.data['house_number'],
+                company=company
+            )
+            contact.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': 'Объет компании не найден'}, status=status.HTTP_400_BAD_REQUEST)
