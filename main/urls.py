2f885ecb047355ce56a9ed7aa089770cbbb15a80
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-04 19:05:59 +0300

    add CompanyDetailAPIView

--- main/urls.py
+++ main/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import CompanyCreateAPIView, CompanyListAPIView, ContactCreateAPIView
+from main.views import (CompanyCreateAPIView, CompanyListAPIView, ContactCreateAPIView,
+                        CompanyDetailAPIView)
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('create/', CompanyCreateAPIView.as_view(), name='create-company'),
     path('list/', CompanyListAPIView.as_view(), name='list-company'),
+    path('company/<int:pk>/', CompanyDetailAPIView.as_view(), name='profile-company'),
 
-
+    #  Contacts urls
     path('contact/create/<int:pk>/', ContactCreateAPIView.as_view(), name='create-contacts'),
 ]
