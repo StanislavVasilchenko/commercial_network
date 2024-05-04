0f78554bce714d6cce05899b5c5138997d47b2e0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-04 21:50:58 +0300

    add CompanyDeleteAPIView in urls

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from main.apps import MainConfig
 from main.views import (CompanyCreateAPIView, CompanyListAPIView, ContactCreateAPIView,
-                        CompanyDetailAPIView, CompanyUpdateAPIView)
+                        CompanyDetailAPIView, CompanyUpdateAPIView, CompanyDeleteAPIView)
 
 app_name = MainConfig.name
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('list/', CompanyListAPIView.as_view(), name='list-company'),
     path('company/<int:pk>/', CompanyDetailAPIView.as_view(), name='profile-company'),
     path('update/<int:pk>/', CompanyUpdateAPIView.as_view(), name='update-company'),
+    path('delete/<int:pk>/', CompanyDeleteAPIView.as_view(), name='delete-company'),
 
     #  Contacts urls
     path('contact/create/<int:pk>/', ContactCreateAPIView.as_view(), name='create-contacts'),
